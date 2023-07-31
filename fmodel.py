fc4e9f7162e19425f39a37986ea67c3e91e9cde2
Sancharika Debnath <80142595+sancharika@users.noreply.github.com>
2023-07-31 20:21:56 +0530

    Update fmodel.py

--- fmodel.py
+++ fmodel.py
@@ -14,55 +14,55 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-def setup_network(structure='vgg16',dropout=0.1,hidden_units=4096, lr=0.001, device='gpu'):
+def setup_network(structure='vgg16', dropout=0.1, hidden_units=4096, lr=0.001, device='gpu'):
     ''' Set up architecture based on input, device and create model,
         returns model and loss
     '''
 
+    device = torch.device("cuda" if torch.cuda.is_available() and device == 'gpu' else 'cpu')
 
-    from collections import OrderedDict
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if structure == 'vgg16':
         model = models.vgg16(pretrained=True)
     elif structure == 'densenet121':
         model = models.densenet121(pretrained=True)
-    
+
     for para in model.parameters():
         para.requires_grad = False
 
-    model.classifier = nn.Sequential(nn.Linear(model.classifier[0].in_features , hidden_units),
-                                     nn.ReLU(),
-                                     nn.Dropout(dropout),
-                                     nn.Linear(hidden_units, 102),
-                                     nn.LogSoftmax(dim=1))
+    model.classifier = nn.Sequential(
+        nn.Linear(model.classifier[0].in_features, hidden_units),
+        nn.ReLU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden_units, 102),
+        nn.LogSoftmax(dim=1)
+    )
+
     model = model.to(device)
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.classifier.parameters(), lr)
-    if torch.cuda.is_available() and device == 'gpu':
-        device = torch.device("cuda:0")
-    else:
-        device = torch.device("cpu")
-    model.to(device)
+
     return model, criterion
 
-def save_checkpoint(train_data, model = 0, path = 'checkpoint.pth', structure = 'vgg16', hidden_units = 4096, dropout = 0.3, lr = 0.001, epochs = 1):
+
+def save_checkpoint(train_data, model, path='checkpoint.pth', structure='vgg16', hidden_units=4096, dropout=0.3, lr=0.001, epochs=1):
     ''' save checkpoint of the PyTorch model
     '''
 
-    model.class_to_idx =  train_data.class_to_idx
-    torch.save({'structure' :structure,
-                'hidden_units':hidden_units,
-                'dropout':dropout,
-                'learning_rate':lr,
-                'no_of_epochs':epochs,
-                'state_dict':model.state_dict(),
-                'class_to_idx':model.class_to_idx},
-                path)
-    
-def load_checkpoint(path = 'checkpoint.pth'):
+    model.class_to_idx = train_data.class_to_idx
+    torch.save({
+        'structure': structure,
+        'hidden_units': hidden_units,
+        'dropout': dropout,
+        'learning_rate': lr,
+        'no_of_epochs': epochs,
+        'state_dict': model.state_dict(),
+        'class_to_idx': model.class_to_idx
+    }, path)
+
+
+def load_checkpoint(path='checkpoint.pth'):
     ''' Create and load checkpoint for a PyTorch model,
-        returns the pytorch model
+        returns the PyTorch model
     '''
 
     checkpoint = torch.load(path)
@@ -76,31 +76,44 @@ def load_checkpoint(path = 'checkpoint.pth'):
     model.load_state_dict(checkpoint['state_dict'])
     return model
 
-def predict(image_path, model, topk=5, device='gpu'):   
+
+def predict(image_path, model, topk=5, device='gpu'):
     ''' Evaluate the a PyTorch model
     '''
-    model.to('cuda')
+
+    device = torch.device("cuda" if torch.cuda.is_available() and device == 'gpu' else 'cpu')
+    model.to(device)
     model.eval()
-    img = process_image(image_path).numpy()
-    img = torch.from_numpy(np.array([img])).float()
     with torch.no_grad():
-        output = model.forward(img.cuda())
-    probs = torch.exp(output).data
-    return probs.topk(topk)
+        img = process_image(image_path).unsqueeze(0).to(device)
+        output = model.forward(img)
+        probs = torch.exp(output).cpu().data
+        top_probs, top_idx = probs.topk(topk)
+        top_probs = top_probs.numpy().squeeze()
+        top_idx = top_idx.numpy().squeeze()
+        class_to_idx_inv = {v: k for k, v in model.class_to_idx.items()}
+        top_classes = [class_to_idx_inv[idx] for idx in top_idx]
+
+    return top_probs, top_classes
 
 
 def process_image(image):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
-        returns an Numpy array
+        returns a PyTorch tensor
     '''
-    
-    img_pil = Image.open(image)
-    img_transforms = transforms.Compose([transforms.Resize(256),
-                                         transforms.CenterCrop(224),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize([0.485, 0.456, 0.406],
-                                                              [0.229, 0.224, 0.225])])
-    
-    image = img_transforms(img_pil)
 
+    img_pil = Image.open(image)
+    img_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    image = img_transforms(img_pil)
     return image
+
+# Usage example:
+if __name__ == "__main__":
+    model, criterion = setup_network(structure='vgg16', dropout=0.3, hidden_units=512, lr=0.001, device='gpu')
+    print(model)
